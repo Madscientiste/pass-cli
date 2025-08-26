0c0fba6b5a5ff2ace7f9d780a5eb205063bffb5c
Carlos Quintana <carlos.quintana@proton.ch>
2025-08-26 15:26:02 +0200

    test: add tests for view item

--- pass/src/test_tools/helpers.rs
+++ pass/src/test_tools/helpers.rs
@@ -1,9 +1,16 @@
+use crate::item::item_keys::DecryptedItemKey;
+use crate::item::list::ItemRevision;
 use crate::share::keys::{ShareKeyList, ShareKeyResponse};
 use crate::test_tools::{MuonServerExt, TEST_ADDRESS_ID, success};
 use muon::Method;
 use muon::test::server::Server;
-use pass_domain::TargetType;
+use pass_domain::{
+    CustomItem, CustomSection, ItemContent, ItemData, ItemExtraField, ItemExtraFieldContent,
+    TargetType, crypto,
+};
+use rand::Rng;
 use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
 
 #[macro_export]
 macro_rules! share_id {
@@ -77,4 +84,154 @@ pub fn setup_share_keys(server: &Arc<Server>, share_id: &str) {
             })
         },
     );
+}
+
+pub fn setup_item_revision(
+    server: &Arc<Server>,
+    share_id: &str,
+    item_id: &str,
+    item_revision: ItemRevision,
+) -> Arc<AtomicBool> {
+    server.handler_with_method(
+        Method::GET,
+        format!("/pass/v1/share/{share_id}/item/{item_id}"),
+        move |_| {
+            success(crate::item::get_one::GetItemResponse {
+                item: item_revision.clone(),
+            })
+        },
+    )
+}
+
+pub struct ItemRevisionBuilder {
+    item_id: String,
+    revision: Option<u64>,
+    content_format_version: Option<i32>,
+    content: Option<String>,
+    item_key: Option<Option<String>>,
+    state: Option<u8>,
+    flags: Option<u64>,
+    alias_email: Option<Option<String>>,
+}
+
+#[allow(dead_code)]
+impl ItemRevisionBuilder {
+    pub fn new(item_id: String) -> Self {
+        Self {
+            item_id,
+            revision: None,
+            content_format_version: None,
+            content: None,
+            item_key: None,
+            state: None,
+            flags: None,
+            alias_email: None,
+        }
+    }
+
+    pub fn with_revision(mut self, value: u64) -> Self {
+        self.revision = Some(value);
+        self
+    }
+    pub fn with_content_format_version(mut self, value: i32) -> Self {
+        self.content_format_version = Some(value);
+        self
+    }
+    pub fn with_content(mut self, value: String) -> Self {
+        self.content = Some(value);
+        self
+    }
+    pub fn with_item_key(mut self, value: Option<String>) -> Self {
+        self.item_key = Some(value);
+        self
+    }
+    pub fn with_state(mut self, value: u8) -> Self {
+        self.state = Some(value);
+        self
+    }
+    pub fn with_flags(mut self, value: u64) -> Self {
+        self.flags = Some(value);
+        self
+    }
+    pub fn with_alias_email(mut self, value: Option<String>) -> Self {
+        self.alias_email = Some(value);
+        self
+    }
+
+    pub fn build(self) -> ItemRevision {
+        ItemRevision {
+            item_id: self.item_id,
+            revision: self.revision.unwrap_or(1),
+            content_format_version: self.content_format_version.unwrap_or(1),
+            key_rotation: 1,
+            content: self.content.unwrap_or_default(),
+            item_key: self.item_key.unwrap_or(None),
+            state: self.state.unwrap_or(1),
+            flags: self.flags.unwrap_or(0),
+            alias_email: self.alias_email.unwrap_or(None),
+        }
+    }
+}
+
+pub fn random_string(length: usize) -> String {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".as_bytes();
+
+    let mut res = String::new();
+    while res.len() < length {
+        let idx = rand::rng().random_range(0..chars.len());
+        res.push(chars[idx] as char);
+    }
+
+    res
+}
+
+pub fn encrypt_for_vault_key(data: &[u8], tag: crypto::EncryptionTag) -> Vec<u8> {
+    crypto::encrypt(data, &TEST_SHARE_KEY, tag).expect("encrypt data failed")
+}
+
+#[allow(dead_code)]
+pub fn decrypt_for_vault_key(ciphertext: &[u8], tag: crypto::EncryptionTag) -> Vec<u8> {
+    crypto::decrypt(ciphertext, &TEST_SHARE_KEY, tag).expect("decrypt data failed")
+}
+
+#[allow(dead_code)]
+pub struct EncryptItemContentsResult {
+    pub item_key: DecryptedItemKey,
+    pub encrypted_item_key: Vec<u8>,
+    pub encrypted_contents: Vec<u8>,
+}
+
+pub fn encrypt_item_contents(data: ItemData) -> EncryptItemContentsResult {
+    let serialized = data.serialize().expect("serialize data failed");
+    let item_key = crypto::generate_encryption_key();
+    let encrypted_data =
+        crypto::encrypt(&serialized, &item_key, crypto::EncryptionTag::ItemContent)
+            .expect("Error encrypting item content");
+
+    let encrypted_item_key = encrypt_for_vault_key(&item_key, crypto::EncryptionTag::ItemKey);
+
+    EncryptItemContentsResult {
+        item_key: DecryptedItemKey(item_key),
+        encrypted_item_key,
+        encrypted_contents: encrypted_data,
+    }
+}
+
+pub fn create_random_item() -> ItemData {
+    ItemData::new(
+        random_string(10),
+        random_string(10),
+        random_string(10),
+        ItemContent::Custom(CustomItem {
+            sections: vec![CustomSection {
+                section_name: random_string(10),
+                section_fields: vec![ItemExtraField {
+                    name: random_string(10),
+                    content: ItemExtraFieldContent::Text(random_string(10)),
+                }],
+            }],
+        }),
+        vec![],
+    )
+    .expect("Error creating item data")
 }

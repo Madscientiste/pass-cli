bf27d5d2d275c1b1d210ace9e47a0c28e286cee3
Carlos Quintana <carlos.quintana@proton.ch>
2025-10-22 16:28:28 +0200

    style: fix lint

--- pass-cli/build.rs
+++ pass-cli/build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 fn main() {
     // Get the git hash
     let git_hash = Command::new("git")
-        .args(&["rev-parse", "--short", "HEAD"])
+        .args(["rev-parse", "--short", "HEAD"])
         .output()
         .ok()
         .and_then(|output| {
@@ -17,9 +17,8 @@ fn main() {
         .unwrap_or_else(|| "unknown".to_string());
 
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
-    
+
     // Re-run the build script if the git HEAD or index changes
     println!("cargo:rerun-if-changed=../.git/HEAD");
     println!("cargo:rerun-if-changed=../.git/index");
 }
-

afb9cad09753ba1e348f1251f7232126ba6f674f
Carlos Quintana <carlos.quintana@proton.ch>
2026-04-02 16:10:06 +0200

    chore: only request PATs for Pass

--- pass/src/personal_access_token/list.rs
+++ pass/src/personal_access_token/list.rs
@@ -59,7 +59,8 @@ impl PassClient {
 
         loop {
             let mut req = GET!("/account/v4/personal-access-token")
-                .query(("PageSize", format!("{}", PAGE_SIZE)));
+                .query(("PageSize", format!("{}", PAGE_SIZE)))
+                .query(("Product", "pass".to_string()));
 
             if let Some(token) = &last_token {
                 req = req.query(("Since", token.clone()));
